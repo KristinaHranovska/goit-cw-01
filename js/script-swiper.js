5301ee39313be3c19b0f21f65256a5160de3e701
Kristina <kr.hranovska@gmail.com>
2023-09-13 13:54:39 +0300

    add review window

---
+++ js/script-swiper.js
@@ -0,0 +1,81 @@
+// slider for products
+$(document).ready(function () {
+        const productsList = $('.products-list');
+
+        productsList.on('afterChange', function (event, slick, currentSlide) {
+          const dots = $('.product-slider-dots li');
+          dots.removeClass('active');
+          dots.eq(currentSlide).addClass('active');
+        });
+
+        productsList.slick({
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          nextArrow: '.swiper-button-next',
+          prevArrow: '.swiper-button-prev',
+          autoplay: true, // Додано автопрокрутку
+          autoplaySpeed: 3000, // Інтервал автопрокрутки у мілісекундах (1 секунда = 1000 мілісекунд)
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+          ],
+        });
+
+        const dots = $('.product-slider-dots li');
+
+        dots.on('click', function () {
+          const index = $(this).index();
+          productsList.slick('slickGoTo', index);
+        });
+ });
+      
+// slider for feedback
+  $(document).ready(function () {
+        const feedbackList = $('.feedback-list');
+
+        feedbackList.on('afterChange', function (event, slick, currentSlide) {
+          const dots = $('.feedback-slider-dots li');
+          dots.removeClass('active');
+          dots.eq(currentSlide).addClass('active');
+        });
+
+        feedbackList.slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          nextArrow: '.swiper-feedback-button-next',
+          prevArrow: '.swiper-feedback-button-prev',
+          autoplay: true, // Додано автопрокрутку
+          autoplaySpeed: 3000, // Інтервал автопрокрутки у мілісекундах (1 секунда = 1000 мілісекунд)
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+          ],
+        });
+
+        const dots = $('.feedback-slider-dots li');
+
+        dots.on('click', function () {
+          const index = $(this).index();
+          feedbackList.slick('slickGoTo', index);
+        });
+  });
--- js/script.js
+++ js/script.js
@@ -1,86 +1,3 @@
-// slider for products
-$(document).ready(function () {
-        const productsList = $('.products-list');
-
-        productsList.on('afterChange', function (event, slick, currentSlide) {
-          const dots = $('.product-slider-dots li');
-          dots.removeClass('active');
-          dots.eq(currentSlide).addClass('active');
-        });
-
-        productsList.slick({
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          nextArrow: '.swiper-button-next',
-          prevArrow: '.swiper-button-prev',
-          autoplay: true, // Додано автопрокрутку
-          autoplaySpeed: 3000, // Інтервал автопрокрутки у мілісекундах (1 секунда = 1000 мілісекунд)
-          responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2,
-              },
-            },
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-              },
-            },
-          ],
-        });
-
-        const dots = $('.product-slider-dots li');
-
-        dots.on('click', function () {
-          const index = $(this).index();
-          productsList.slick('slickGoTo', index);
-        });
- });
-      
-// slider for feedback
-  $(document).ready(function () {
-        const feedbackList = $('.feedback-list');
-
-        feedbackList.on('afterChange', function (event, slick, currentSlide) {
-          const dots = $('.feedback-slider-dots li');
-          dots.removeClass('active');
-          dots.eq(currentSlide).addClass('active');
-        });
-
-        feedbackList.slick({
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          nextArrow: '.swiper-feedback-button-next',
-          prevArrow: '.swiper-feedback-button-prev',
-          autoplay: true, // Додано автопрокрутку
-          autoplaySpeed: 3000, // Інтервал автопрокрутки у мілісекундах (1 секунда = 1000 мілісекунд)
-          responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2,
-              },
-            },
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-              },
-            },
-          ],
-        });
-
-        const dots = $('.feedback-slider-dots li');
-
-        dots.on('click', function () {
-          const index = $(this).index();
-          feedbackList.slick('slickGoTo', index);
-        });
-  });
-      
-
   // script to make a modal window appear
   (() => {
   const refs = {
@@ -96,3 +13,59 @@ $(document).ready(function () {
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+// script to make a review window appear
+  
+  (() => {
+  const refs = {
+    openReviewBtn: document.querySelector("[data-review-open]"),
+    closeReviewBtn: document.querySelector("[data-review-close]"),
+    review: document.querySelector("[data-review]"),
+  };
+
+  refs.openReviewBtn.addEventListener("click", toggleReview);
+  refs.closeReviewBtn.addEventListener("click", toggleReview);
+
+  function toggleReview() {
+    refs.review.classList.toggle("is-hidden");
+  }
+})();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var submitButton = document.querySelector('.submit-form-review');
+    var form = document.querySelector('.form-review');
+
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Цей рядок запобігає стандартній поведінці кнопки submit
+        // Тут можна додатковий код для обробки форми, якщо потрібно
+    });
+
+});
+
+// up arrow settings
+
+document.addEventListener('DOMContentLoaded', function() {
+    var upButton = document.querySelector('.up-btn');
+
+    upButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+
+        document.body.classList.add('scrolling');
+    });
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            upButton.classList.add('show');
+        } else {
+            upButton.classList.remove('show');
+        }
+
+        if (document.body.classList.contains('scrolling') && window.scrollY === 0) {
+            document.body.classList.remove('scrolling');
+        }
+    });
+});
+
