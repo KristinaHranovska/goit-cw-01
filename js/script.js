9256150597219a9b61ee418f204f3247cf255207
Kristina <kr.hranovska@gmail.com>
2023-09-13 18:33:51 +0300

    add buy now modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-  // script to make a modal window appear
+//script to make a buy now window appear
+    (() => {
+  const refs = {
+    openBuyBtn: document.querySelector("[data-buy-open]"),
+    openBuyAddBtn: document.querySelector("[data-buy-add-open]"),
+    closeBuyBtn: document.querySelector("[data-buy-close]"),
+    buy: document.querySelector("[data-buy]"),
+  };
+
+  refs.openBuyBtn.addEventListener("click", toggleModal);
+  refs.openBuyAddBtn.addEventListener("click", toggleModal);
+  refs.closeBuyBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.buy.classList.toggle("is-hidden");
+  }
+})();
+
+// preventing standard behavior (window closing + refresh)
+document.addEventListener('DOMContentLoaded', function() {
+    var submitButton = document.querySelector('.submit-form-buy');
+    var form = document.querySelector('.form-buy');
+
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Цей рядок запобігає стандартній поведінці кнопки submit
+    });
+
+});
+
+
+// script to make a modal window appear
   (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -14,8 +44,19 @@
   }
 })();
 
+// preventing standard behavior (window closing + refresh)
+document.addEventListener('DOMContentLoaded', function() {
+    var submitButton = document.querySelector('.submit-form-modal');
+    var form = document.querySelector('.form-modal');
+
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Цей рядок запобігає стандартній поведінці кнопки submit
+    });
+
+});
+
+
 // script to make a review window appear
-  
   (() => {
   const refs = {
     openReviewBtn: document.querySelector("[data-review-open]"),
@@ -31,19 +72,19 @@
   }
 })();
 
+// preventing standard behavior (window closing + refresh)
 document.addEventListener('DOMContentLoaded', function() {
     var submitButton = document.querySelector('.submit-form-review');
     var form = document.querySelector('.form-review');
 
     submitButton.addEventListener('click', function(event) {
         event.preventDefault(); // Цей рядок запобігає стандартній поведінці кнопки submit
-        // Тут можна додатковий код для обробки форми, якщо потрібно
     });
 
 });
 
-// up arrow settings
 
+// up arrow settings
 document.addEventListener('DOMContentLoaded', function() {
     var upButton = document.querySelector('.up-btn');
 
@@ -69,3 +110,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+// блокування вводу букв в поле (тільки цифри)
+document.addEventListener('DOMContentLoaded', function() {
+    var cardInput = document.querySelector('.number-input[type="text"]');
+    cardInput.addEventListener('keydown', function(e) {
+        if (!/^\d+$/.test(e.key) && e.key !== 'Backspace' && e.key !== 'Delete' && e.key !== 'ArrowLeft' && e.key !== 'ArrowRight') {
+            e.preventDefault();
+        }
+    });
+});
